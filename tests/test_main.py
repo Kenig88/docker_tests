1c834b7edfd38d1c8779c9f42a076ff6bf2092d5
keni <14bass@kk.ri>
2026-01-13 15:52:48 +0300

    Complete tests

---
+++ tests/test_main.py
@@ -0,0 +1,21 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import pytest
+
+
+@pytest.fixture(autouse=True)
+def driver():
+    driver = webdriver.Chrome()
+    yield driver
+    driver.quit()
+
+
+
+def test_inputs(driver):
+    driver.get("https://the-internet.herokuapp.com/inputs")
+    assert driver.find_element("xpath", "//h3[text()='Inputs']").text == "Inputs"
+
+
+def test_context_menu(driver):
+    driver.get("https://the-internet.herokuapp.com/context_menu")
+    assert driver.find_element("xpath", "//h3[text()='Context Menu']").text == "Context Menu"
